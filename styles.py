7bc1bcb1f352d277dec3dd4c5994bdee8b044fe6
Igor Kantar <igor_kan_bl@hotmail.com>
2022-06-09 11:04:12 +0000

    changed

--- styles.py
+++ styles.py
@@ -1,4 +1,8 @@
-textInput = """
-    max-width:50px;
-    border :3px solid black;
+qss1 = """
+    max-width:400px;
+    min-width:300px;
+    border :3px solid darkblue;
+"""
+qss2 = """
+    border :3px solid darkblue;
 """
