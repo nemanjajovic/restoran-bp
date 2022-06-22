5fcee4b939e49bca62fbd5a2d1511c2f2d1082eb
Igor Kantar <igor_kan_bl@hotmail.com>
2022-06-22 13:32:39 +0200

    Added connection context manager and database handler class

--- db.py
+++ db.py
@@ -1,34 +1,34 @@
-from os import getenv
-import pymssql
+import pyodbc
 
-server = getenv("PYMSSQL_TEST_SERVER")
-user = getenv("PYMSSQL_TEST_USERNAME")
-password = getenv("PYMSSQL_TEST_PASSWORD")
+class DbHandler:
+    def __init__(self):
+        self.conn = pyodbc.connect(
+            "Driver={SQL Server Native Client 11.0};"
+            "Server=DESKTOP-1P60H2U;"
+            "Database=Test;"
+            "Trusted_Connection=yes;"
+        )
+        self.cursor = self.conn.cursor()
 
-conn = pymssql.connect(server, user, password, "tempdb")
-cursor = conn.cursor()
-cursor.execute("""
-IF OBJECT_ID('persons', 'U') IS NOT NULL
-    DROP TABLE persons
-CREATE TABLE persons (
-    id INT NOT NULL,
-    name VARCHAR(100),
-    salesrep VARCHAR(100),
-    PRIMARY KEY(id)
-)
-""")
-cursor.executemany(
-    "INSERT INTO persons VALUES (%d, %s, %s)",
-    [(1, 'John Smith', 'John Doe'),
-     (2, 'Jane Doe', 'Joe Dog'),
-     (3, 'Mike T.', 'Sarah H.')])
-# you must call commit() to persist your data if you don't set autocommit to True
-conn.commit()
+    def select(self, columns, table, condition):
+        extend = f"WHERE {condition}" if condition else ""
+        rows = self.cursor.execute(f"""SELECT {columns} FROM {table} {extend};""")
+        for row in rows:
+            print("ID=%d, Name=%s Position=%s" % (row[0], row[1], row[2]))
 
-cursor.execute('SELECT * FROM persons WHERE salesrep=%s', 'John Doe')
-row = cursor.fetchone()
-while row:
-    print("ID=%d, Name=%s" % (row[0], row[1]))
-    row = cursor.fetchone()
+# context manager
+class Connection:
+    def __init__(self):
+        pass
 
-conn.close()
+    def __enter__(self):
+        self.handler = DbHandler()
+        return self.handler
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.handler:
+            self.handler.conn.close()
+
+# context manager test
+with Connection() as handler:
+    handler.select("*", "Radnici","")
