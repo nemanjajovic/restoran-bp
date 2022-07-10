45423a071e7dd02058b6796706b951141dc344a5
Igor Kantar <igor_kan_bl@hotmail.com>
2022-07-10 18:10:16 +0200

    Added allowed worker categories

--- data.py
+++ data.py
@@ -1,4 +1,9 @@
 itemCategories = [
-            ["Topli Napitak","Sok","Pivo","Vino","Koktel","Zestoko Pice","Voda"], # drinks
-            ["Meso","Riba","Salata","Dezert","Prilog"],      # food
-        ]
+    ["Topli Napitak","Sok","Pivo","Vino","Koktel","Zestoko Pice","Voda"], # drinks
+    ["Meso","Riba","Salata","Dezert","Prilog"],      # food
+]
+
+workerCategories = [
+    ['Sef Smjene', 'Konobar', 'Runner', 'Sanker'],   # Service
+    ['Sef Kuhinje',  'Friteza', 'Gril', 'Salate/Dezerti', 'Burgeri', 'Sudje', 'Pripreme',],     # Kitchen
+]
